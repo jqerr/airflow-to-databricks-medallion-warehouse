8d8fd1aef4387c74f1781ebae0e1774e8e15b068
jqerr <1421083220@qq.com>
2026-07-01 23:58:06 +1000

    Sync birthdate cutoff in quality_checks_silver.sql with DAG check

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -120,11 +120,11 @@ ORDER BY sls_sales, sls_quantity, sls_price;
 -- Checking 'silver.erp_cust_az12'
 -- ====================================================================
 -- Identify Out-of-Range Dates
--- Expectation: Birthdates between 1924-01-01 and Today
-SELECT DISTINCT 
-    bdate 
+-- Expectation: Birthdates between 1900-01-01 and Today
+SELECT DISTINCT
+    bdate
 FROM silver.erp_cust_az12
-WHERE bdate < '1924-01-01' 
+WHERE bdate < '1900-01-01'
    OR bdate > GETDATE();
 
 -- Data Standardization & Consistency
